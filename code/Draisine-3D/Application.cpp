1125af6447932501526fb8b7339d721f14782837
Michał Kowieski <kowieski.m98@gmail.com>
2019-12-28 00:56:36 +0100

    Zwiększono złożoność przykładowej aplikacji

--- code/Draisine-3D/Application.cpp
+++ code/Draisine-3D/Application.cpp
@@ -1,41 +1,28 @@
 #define GLEW_STATIC
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <SOIL.h>
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <string>
+using namespace std;
 
 const GLuint WIDTH = 800, HEIGHT = 600;
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
-	std::cout << key << std::endl;
+	cout << key << endl;
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
-
 int main()
 {
-	std::ifstream fragmentShaderFile("shader.frag");
-	std::stringstream fragmentShaderStream;
-	fragmentShaderStream << fragmentShaderFile.rdbuf();
-	std::string fragmentShaderString = fragmentShaderStream.str();
-	const GLchar* fragmentShaderSource = fragmentShaderString.c_str();
-
-
-	std::ifstream vertexShaderFile("shader.vert");
-	std::stringstream vertexShaderStream;
-	vertexShaderStream << vertexShaderFile.rdbuf();
-	std::string vertexShaderString = vertexShaderStream.str();
-	const GLchar* vertexShaderSource = vertexShaderString.c_str();
-
-
 	if (glfwInit() != GL_TRUE)
 	{
-		std::cout << "GLFW initialization failed" << std::endl;
+		cout << "GLFW initialization failed" << endl;
 		return -1;
 	}
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -44,17 +31,39 @@ int main()
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 	try
 	{
-		GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "GKOM - OpenGL 02", nullptr, nullptr);
+		GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "GKOM - OpenGL 04", nullptr, nullptr);
 		if (window == nullptr)
-			throw std::exception("GLFW window not created");
+			throw exception("GLFW window not created");
 		glfwMakeContextCurrent(window);
 		glfwSetKeyCallback(window, key_callback);
 
 		glewExperimental = GL_TRUE;
 		if (glewInit() != GLEW_OK)
-			throw std::exception("GLEW Initialization failed");
+			throw exception("GLEW Initialization failed");
 
 		glViewport(0, 0, WIDTH, HEIGHT);
+
+		// Let's check what are maximum parameters counts
+		GLint nrAttributes;
+		glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+		cout << "Max vertex attributes allowed: " << nrAttributes << std::endl;
+		glGetIntegerv(GL_MAX_TEXTURE_COORDS, &nrAttributes);
+		cout << "Max texture coords allowed: " << nrAttributes << std::endl;
+
+
+
+		// Build, compile and link shader program
+		std::ifstream vertexShaderFile("shader.vert");
+		std::stringstream vertexShaderStream;
+		vertexShaderStream << vertexShaderFile.rdbuf();
+		std::string vertexShaderString = vertexShaderStream.str();
+		const GLchar* vertexShaderSource = vertexShaderString.c_str();
+
+		std::ifstream fragmentShaderFile("shader.frag");
+		std::stringstream fragmentShaderStream;
+		fragmentShaderStream << fragmentShaderFile.rdbuf();
+		std::string fragmentShaderString = fragmentShaderStream.str();
+		const GLchar* fragmentShaderSource = fragmentShaderString.c_str();
 
 		// Build and compile our shader program
 		// Vertex shader
@@ -68,8 +77,8 @@ int main()
 		if (!success)
 		{
 			glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-			std::string msg = std::string("Vertex shader compilation: ") + infoLog;
-			throw std::exception(msg.c_str());
+			string msg = string("Vertex shader compilation: ") + infoLog;
+			throw exception(msg.c_str());
 		}
 
 		// Fragment shader
@@ -81,9 +90,10 @@ int main()
 		if (!success)
 		{
 			glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-			std::string msg = std::string("Fragment shader compilation: ") + infoLog;
-			throw std::exception(msg.c_str());
+			string msg = string("Fragment shader compilation: ") + infoLog;
+			throw exception(msg.c_str());
 		}
+
 
 		// Link shaders
 		GLuint shaderProgram = glCreateProgram();
@@ -94,41 +104,83 @@ int main()
 		glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
 		if (!success) {
 			glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-			std::string msg = std::string("Shader linking: ") + infoLog;
-			throw std::exception(msg.c_str());
+			string msg = string("Shader linking: ") + infoLog;
+			throw exception(msg.c_str());
 		}
+
 		glDeleteShader(vertexShader);
 		glDeleteShader(fragmentShader);
 
-		// Set up vertex data (and buffer(s)) and attribute pointers
+
+		// Set up vertex data 
 		GLfloat vertices[] = {
-			0.5f, 0.5f, 0.0f,
-			-0.5f, 0.5f, 0.0f,
-			0.0f, -0.5f, 0.0f,
+			// coordinates			// color			// texture
+			 0.25f,  0.5f,  0.0f,	1.0f, 0.0f, 0.0f,	1.0f,  0.0f,
+			-0.75f,  0.5f,  0.0f,	0.0f, 1.0f, 0.0f,	0.0f,  0.0f,
+			-0.25f, -0.5f,  0.0f,	0.0f, 0.0f, 1.0f,	0.0f,  1.0f,
+			 0.75f, -0.5f,  0.0f,	1.0f, 0.0f, 1.0f,	1.0f,  1.0f,
 		};
 
-		GLuint EBO;
-		glGenBuffers(1, &EBO);
+		GLuint indices[] = {
+			0, 1, 2,
+			0, 2, 3,
+		};
 
-		GLuint VBO, VAO;
+		GLuint VBO, EBO, VAO;
 		glGenVertexArrays(1, &VAO);
 		glGenBuffers(1, &VBO);
+		glGenBuffers(1, &EBO);
+
 		// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
 		glBindVertexArray(VAO);
 
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		// vertex geometry data
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
 		glEnableVertexAttribArray(0);
 
+		// vertex color data
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+		glEnableVertexAttribArray(1);
+
+		// vertex texture coordinates
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+		glEnableVertexAttribArray(2);
+
 		glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+
 		glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
 
-		// przyda sie w zadaniu
-		// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		// Set the texture wrapping parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// Set texture wrapping to GL_REPEAT (usually basic wrapping method)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		// Set texture filtering parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-		// main loop
+		// prepare textures
+		int width, height;
+		unsigned char* image = SOIL_load_image("textures/iipw.png", &width, &height, 0, SOIL_LOAD_RGB);
+		if (image == nullptr)
+			throw exception("Failed to load texture file");
+
+		GLuint texture0;
+		glGenTextures(1, &texture0);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture0);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		// freeing unnecessary texture stuff
+		SOIL_free_image_data(image);
+		glBindTexture(GL_TEXTURE_2D, 0);
+
+		// main event loop
 		while (!glfwWindowShouldClose(window))
 		{
 			// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
@@ -138,10 +190,16 @@ int main()
 			glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT);
 
+			// Bind Textures using texture units
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, texture0);
+			glUniform1i(glGetUniformLocation(shaderProgram, "Texture0"), 0);
+
 			// Draw our first triangle
 			glUseProgram(shaderProgram);
+
 			glBindVertexArray(VAO);
-			glDrawArrays(GL_TRIANGLES, 0, _countof(vertices));
+			glDrawElements(GL_TRIANGLES, _countof(indices), GL_UNSIGNED_INT, 0);
 			glBindVertexArray(0);
 
 			// Swap the screen buffers
@@ -149,10 +207,11 @@ int main()
 		}
 		glDeleteVertexArrays(1, &VAO);
 		glDeleteBuffers(1, &VBO);
+		glDeleteBuffers(1, &EBO);
 	}
-	catch (std::exception ex)
+	catch (exception ex)
 	{
-		std::cout << ex.what() << std::endl;
+		cout << ex.what() << endl;
 	}
 	glfwTerminate();
 
--- code/Draisine-3D/shader.frag
+++ code/Draisine-3D/shader.frag
@@ -1,8 +1,13 @@
 #version 330 core
+in vec3 ourColor;
+in vec2 TexCoord;
 
 out vec4 color;
 
+uniform sampler2D Texture0;
+
 void main()
 {
-	color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    //color = mix(texture(Texture0, TexCoord),texture(Texture1, TexCoord),0.4);
+	color = texture(Texture0, TexCoord) * vec4(ourColor, 1.0);
 }
