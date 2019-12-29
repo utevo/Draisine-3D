26e3acebf6fe7e0403ec1204be50b0ed5ac0e380
Michał Kowieski <kowieski.m98@gmail.com>
2019-12-29 23:55:12 +0100

    Dodano funckję GLGetSizeOfType do pliku Utilities.h

--- code/Draisine-3D/Utilities.h
+++ code/Draisine-3D/Utilities.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <assert.h>
+
 #include "Debug.h"
+
 
 #ifdef DEBUG
 #define GLCall(x) GLClearError();\
@@ -9,3 +12,15 @@
 #else
 #define GLCall(x) x
 #endif
+
+static unsigned int GLGetSizeOfType(unsigned int type)
+{
+    switch (type)
+    {
+    case GL_FLOAT: return sizeof(GLfloat);
+    case GL_UNSIGNED_INT: return sizeof(GLuint);
+    case GL_UNSIGNED_BYTE: return sizeof(GLbyte);
+    }
+    ASSERT(false);
+    return 0;
+}
