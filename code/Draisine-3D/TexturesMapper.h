c5268e4a863e0b63b3549311fc6a606556a92603
Michał Kowieski <kowieski.m98@gmail.com>
2020-01-02 21:23:41 +0100

    Aktualizacja klasy TexturesMapper

---
+++ code/Draisine-3D/TexturesMapper.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <vector>
+#include <utility>
+
+#include "Utilities.h"
+#include "Texture.h"
+#include "Shader.h"
+
+
+class TexturesMapper {
+public:
+    TexturesMapper(std::vector <std::pair<std::string, Texture> > &mapping, Shader &shader);
+    ~TexturesMapper();
+
+    void bind() const;
+    void unbind() const;
+
+    std::vector <std::pair<std::string, Texture> > getMapping() const { return _mapping; }
+private:
+    std::vector <std::pair<std::string, Texture> > &_mapping;
+    Shader &_shader;
+};
+
