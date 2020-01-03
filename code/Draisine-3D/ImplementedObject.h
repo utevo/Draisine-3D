29f74354c35bc9c0483d28b3023a0a36cebbcd02
Michał Kowieski <kowieski.m98@gmail.com>
2020-01-03 18:00:45 +0100

    Stworzenie klasy ImplementedObject

---
+++ code/Draisine-3D/ImplementedObject.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Object.h"
+#include "VertexArray.h"
+#include "IndexBuffer.h"
+#include "Shader.h"
+#include "TexturesMapper.h"
+
+
+class ImplementedObject : public Object {
+public:
+	ImplementedObject() {};
+	void render() {};
+
+private:
+	
+
+};
+
