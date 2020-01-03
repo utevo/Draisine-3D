e6cb955f7ce14a869e7c8893642cae7392bb1f8e
Michał Kowieski <kowieski.m98@gmail.com>
2020-01-03 16:30:44 +0100

    Stworzono klasę OrtogonalProjection

---
+++ code/Draisine-3D/OrtogonalProjection.cpp
@@ -0,0 +1,22 @@
+#include "OrtogonalProjection.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "Projection.h"
+
+
+OrtogonalProjection::OrtogonalProjection(float left, float right, float bottom, float top) {
+	_matrix = glm::ortho(left, right, bottom, top);
+}
+
+OrtogonalProjection::OrtogonalProjection(float left, float right, float bottom, float top, float zNear, float zFar) {
+	_matrix = glm::ortho(left, right, bottom, top, zNear, zFar);
+}
+
+OrtogonalProjection::~OrtogonalProjection() {
+}
+
+glm::mat4& OrtogonalProjection::getMatrix() {
+	return _matrix;
+}
