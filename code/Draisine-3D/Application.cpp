6be9edfd1444bf11a4d20ca8a11c3573ebb1503f
Denys Klochkov <4234973@gmail.com>
2020-01-06 19:27:09 +0100

    Stworzono objekt 3D, zaimplementowana logika ruchu oraz rotacji kamery [issue 33]

--- code/Draisine-3D/Application.cpp
+++ code/Draisine-3D/Application.cpp
@@ -18,36 +18,46 @@
 #include "TexturesMapper.h"
 
 #include "objects/Square.h"
+#include "objects/Ground.h"
+#include "objects/ThreeD_Obj.h"
 
 #include "OrtogonalProjection.h"
 #include "PerspectiveProjection.h"
 #include "PositionFrontUpView.h"
 
 
+
 using namespace std;
 
 const GLuint WIDTH = 800, HEIGHT = 600;
 
+glm::mat4 rotationMat(1);
+glm::mat4 quarterRotation(1);
+
 glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 1.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f,-1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
 float step = 0.1;
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
 	// cout << key << endl;
 
-	if (key == GLFW_KEY_UP && action == GLFW_PRESS)
-		cameraPos[2] -= step;
-	if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
-		cameraPos[2] += step;
-	if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
-		cameraPos[0] += step;
-	if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
-		cameraPos[0] -= step;
-	if (key == GLFW_KEY_Q && action == GLFW_PRESS)
-		cameraPos[1] -= step;
-	if (key == GLFW_KEY_E && action == GLFW_PRESS)
-		cameraPos[1] += step;
-
+	if (key == GLFW_KEY_UP && action == GLFW_PRESS)	//STEP_FORWARD
+		cameraPos += step*cameraFront;
+	if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)//STEP_BACK
+		cameraPos -= step*cameraFront;
+	if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)//STEP_RIGHT
+		cameraPos += step * glm::vec3( quarterRotation * glm::vec4(cameraFront,1.0));
+	if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)//STEP_LEFT
+		cameraPos += step * glm::vec3(glm::inverse(quarterRotation) * glm::vec4(cameraFront, 1.0));
+	if (key == GLFW_KEY_Q && action == GLFW_PRESS)	//ROTATE_COUNTER_CLOCKWISE
+		cameraFront=glm::vec3(rotationMat * glm::vec4(cameraFront, 1.0));
+	
+	if (key == GLFW_KEY_E && action == GLFW_PRESS)	//ROTATE_CLOCKWISE
+		cameraFront = glm::vec3(glm::inverse(rotationMat)*glm::vec4(cameraFront, 1.0));
+	
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
 }
@@ -83,12 +93,14 @@ int main()
 		glfwSetKeyCallback(window, key_callback);
 
 		glewExperimental = GL_TRUE;
+		
+		glEnable(GL_DEPTH_TEST);
+
 		if (glewInit() != GLEW_OK)
 			throw exception("GLEW Initialization failed");
 
 
-		glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-		glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+	
 		std::shared_ptr<PositionFrontUpView> view = std::make_shared<PositionFrontUpView>(cameraPos, cameraFront, cameraUp);
 		//std::shared_ptr<OrtogonalProjection> projection = std::make_shared<OrtogonalProjection>(-2.0, 2.0, -2.0, 2.0, -2.0, 2.0);
 		float fov = 45.f;
@@ -98,16 +110,25 @@ int main()
 		Renderer renderer = Renderer(*view, *projection);
 
 		Square square = Square();
-
+		Ground gr = Ground();
+		ThreeD_Obj cube = ThreeD_Obj();
+		rotationMat = glm::rotate(rotationMat, 0.1f, glm::vec3(0.0, 1.0, 0.0));
+		quarterRotation= glm::rotate(rotationMat, -3.14152f/2 , glm::vec3(0.0, 1.0, 0.0));
+		//
 		// main event loop
 		while (!glfwWindowShouldClose(window)) {
 			// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
 			glfwPollEvents();
 
 			renderer.clear();
-			square.render(renderer);
-			view->setPosition(cameraPos);
 
+
+			view->setPosition(cameraPos);
+			view->setFront(cameraFront);
+
+			//square.render(renderer);
+			gr.render(renderer);
+			cube.render(renderer);
 			// Swap the screen buffers
 			glfwSwapBuffers(window);
 		}
