0fc4239e24d8be3fbbc51f767479b19e377fa8a2
Michał Kowieski <kowieski.m98@gmail.com>
2020-01-07 17:36:52 +0100

    Zaktualizowano klasy Object oraz CompositeObject

--- code/Draisine-3D/CompositeObject.h
+++ code/Draisine-3D/CompositeObject.h
@@ -7,15 +7,30 @@
 
 class CompositeObject : public Object {
 public:
-	virtual void render() {
+	virtual void render(Renderer& renderer) {
 		for (Object& object : _objects)
-			object.render();
+			object.render(renderer);
 	}
 
-	void add(Object& object) {
+	void addChild(Object& object) {
 		_objects.push_back(object);
 	}
+	void removeChild(unsigned int number) {
+		_objects.erase(_objects.begin() + number);
+	}
 
+	virtual void move(const glm::vec3& vec) {
+		for (Object& object : _objects)
+			object.move(vec);
+	}
+	virtual void scale(const glm::vec3& vec) {
+		for (Object& object : _objects)
+			object.scale(vec);
+	}
+	virtual void rotate(const glm::vec3& vec) {
+		for (Object& object : _objects)
+			object.rotate(vec);
+	}
 private:
 	std::vector <Object&> _objects;
 };
