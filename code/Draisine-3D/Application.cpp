91221ca70b1bc7aa73d9adb1aa7a600188472eed
Denys Klochkov <4234973@gmail.com>
2020-01-09 17:27:01 +0100

    Stworzono plik naglowkowy Camera.h

--- code/Draisine-3D/Application.cpp
+++ code/Draisine-3D/Application.cpp
@@ -9,6 +9,9 @@
 #include <sstream>
 #include <memory>
 
+
+#include "Camera.h"
+
 #include "Utilities.h"
 #include "VertexArray.h"
 #include "VertexBuffer.h"
@@ -29,37 +32,40 @@ using namespace std;
 
 const GLuint WIDTH = 800, HEIGHT = 600;
 
-glm::mat4 rotationMat(1);
-glm::mat4 quarterRotation(1);
-
 glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 1.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f,-1.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+Camera cam = Camera(cameraPos, cameraFront, cameraUp);;
+float step = 0.005f;//TODO: step should account for fps
+double prev_X, prev_Y;
 
-float step = 0.1;
+void mouse_callback(GLFWwindow* window, double new_X, double new_Y)
+{
+	cam.Rotate_Mouse(prev_X - new_X, prev_Y - new_Y);
+	glfwSetCursorPos(window, prev_X, prev_Y);
+}
 
+//for keys pressed once
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
 	// cout << key << endl;
 
-	if (key == GLFW_KEY_UP && action == GLFW_PRESS)	//STEP_FORWARD
-		cameraPos += step*cameraFront;
-	if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)//STEP_BACK
-		cameraPos -= step*cameraFront;
-	if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)//STEP_RIGHT
-		cameraPos += step * glm::vec3( quarterRotation * glm::vec4(cameraFront,1.0));
-	if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)//STEP_LEFT
-		cameraPos -= step * glm::vec3( quarterRotation * glm::vec4(cameraFront, 1.0));
-	if (key == GLFW_KEY_Q && action == GLFW_PRESS)	//ROTATE_COUNTER_CLOCKWISE
-		cameraFront=glm::vec3(rotationMat * glm::vec4(cameraFront, 1.0));
-	
-	if (key == GLFW_KEY_E && action == GLFW_PRESS)	//ROTATE_CLOCKWISE
-		cameraFront = glm::vec3(glm::inverse(rotationMat)*glm::vec4(cameraFront, 1.0));
-	
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
 }
-
+//for "sticky keys"
+void process_sticky_keys(GLFWwindow* window)
+{
+	//TODO: step should account for fps
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)//STEP_FORWARD
+		cam.Step_Longtitudal(step);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)//STEP_BACK
+		cam.Step_Longtitudal(-step);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)//STEP_LEFT
+		cam.Step_Lateral(-step);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) //STEP_RIGHT
+		cam.Step_Lateral(step);
+}
 void render(const VertexArray& vertexArray, const IndexBuffer& indexBuffer, const Shader& shader) {
 	shader.bind();
 	vertexArray.bind();
@@ -87,8 +93,14 @@ int main()
 		GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "GKOM - OpenGL 04", nullptr, nullptr);
 		if (window == nullptr)
 			throw exception("GLFW window not created");
+
 		glfwMakeContextCurrent(window);
+
+		glfwSetCursorPosCallback(window, mouse_callback);
 		glfwSetKeyCallback(window, key_callback);
+
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+		glfwGetCursorPos(window, &prev_X, &prev_Y);
 
 		glewExperimental = GL_TRUE;
 		
@@ -102,7 +114,8 @@ int main()
 		// std::shared_ptr<OrtogonalProjection> projection = std::make_shared<OrtogonalProjection>(-2.0, 2.0, -2.0, 2.0, -2.0, 2.0);
 		float fov = 45.f;
 		std::shared_ptr<PerspectiveProjection> projection = std::make_shared<PerspectiveProjection>(glm::radians(fov), (float) WIDTH / (float) HEIGHT , 1.0f, 100.0f);
-
+		
+	
 
 		auto texture = std::make_shared<Texture>("textures/iipw.png");
 		auto texture2 = std::make_shared<Texture>("textures/weiti.png");
@@ -112,16 +125,17 @@ int main()
 		auto shader = std::make_shared<Shader>("shader.vert", "shader.frag");
 
 
-		rotationMat = glm::rotate(rotationMat, 0.1f, glm::vec3(0.0, 1.0, 0.0));
-		quarterRotation= glm::rotate(rotationMat, -glm::half_pi<float>(), glm::vec3(0.0, 1.0, 0.0));
 		// main event loop
 		while (!glfwWindowShouldClose(window)) {
 			// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
-			glfwPollEvents();
+
 
 			GLCall(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
+			cameraPos = cam.getPos();
+			cameraFront = cam.getFront();
 			view->setPosition(cameraPos);
 			view->setFront(cameraFront);
+
 			glm::mat4 viewMatrix = view->getMatrix();
 			shader->setUniformMat4("VIEW", viewMatrix);
 			glm::mat4 projectionMatrix = projection->getMatrix();
@@ -130,8 +144,12 @@ int main()
 
 			trapeze.render(shader);
 			trapeze2.render(shader);
+			mouse_callback(window, prev_X, prev_Y);
 
 			glfwSwapBuffers(window);
+			process_sticky_keys(window);
+			glfwPollEvents();
+
 		}
 
 	}
